952735bb49e1a64438ea1f6361ed95464f87c5b3
Rosen Yordanov <roskata182003@gmail.com>
2022-10-06 19:20:29 +0300

    Upload C# Advanced exam from previous course

---
+++ C# Advanced Exams/C# Advanced Retake Exam - 13 April 2022/Blacksmith/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blacksmith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> swords = new Dictionary<string, int>();
+            swords.Add("Gladius", 0);
+            swords.Add("Shamshir", 0);
+            swords.Add("Katana", 0);
+            swords.Add("Sabre", 0);
+            swords.Add("Broadsword", 0);
+            int[] steel = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] carbon = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(steel);
+            Stack<int> stack = new Stack<int>(carbon);
+            while (stack.Any() && queue.Any())
+            {
+                string swordToCraft = string.Empty;
+                int currentSteel = queue.Dequeue();
+                int currentCarbon = stack.Pop();
+                int sum = currentCarbon + currentSteel;
+                if (sum == 70)
+                {
+                    swordToCraft = "Gladius";
+                    swords[swordToCraft]++;
+                }
+                else if (sum == 80)
+                {
+                    swordToCraft = "Shamshir";
+                    swords[swordToCraft]++;
+                }
+                else if (sum == 90)
+                {
+                    swordToCraft = "Katana";
+                    swords[swordToCraft]++;
+                }
+                else if (sum == 110)
+                {
+                    swordToCraft = "Sabre";
+                    swords[swordToCraft]++;
+                }
+                else if (sum == 150)
+                {
+                    swordToCraft = "Broadsword";
+                    swords[swordToCraft]++;
+
+                }
+                else
+                {
+                    currentCarbon += 5;
+                    stack.Push(currentCarbon);
+                }
+            }
+            PrintResult(swords, queue, stack);
+        }
+
+        static void PrintResult(Dictionary<string, int> swords, Queue<int> queue, Stack<int> stack)
+        {
+            int craftedSwordsCount = swords.Values.Sum();
+            if (craftedSwordsCount!=0)
+            {
+                Console.WriteLine($"You have forged {craftedSwordsCount} swords.");
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+            if (queue.Any())
+            {
+                Console.WriteLine($"Steel left: {string.Join(", ",queue)}");
+            }
+            else
+            {
+                Console.WriteLine("Steel left: none");
+            }
+            if (stack.Any())
+            {
+                Console.WriteLine($"Carbon left: {string.Join(", ", stack)}");
+            }
+            else
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+            foreach (KeyValuePair<string,int> sword in swords.Where(x=>x.Value>0).OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{sword.Key}: {sword.Value}");
+            }
+        }
+    }
+}
---
+++ C# Advanced Exams/C# Advanced Retake Exam - 13 April 2022/Drones/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Drones
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Airfield airfield = new Airfield("Heathrow", 10, 10.5);
+            Drone drone = new Drone("D20", "DEERC", 6);
+            Console.WriteLine(drone);
+            Console.WriteLine(airfield.AddDrone(drone));
+            Console.WriteLine(airfield.Count);
+            Console.WriteLine(airfield.RemoveDrone("DE51"));
+            Drone secondDrone = new Drone("CW4", "Cheerwing", 8);
+            Drone thirdDrone = new Drone("X5SW-V3", "Cheerwing", 7);
+            Drone fourthDrone = new Drone("X20", "Cheerwing", 4);
+            Drone fifthDrone = new Drone("EVO2", "Autel", 10);
+            Drone sixtDrone = new Drone("XL5-6S-FPV", "iFlight", 10);
+            Console.WriteLine(airfield.AddDrone(secondDrone));
+            Console.WriteLine(airfield.AddDrone(thirdDrone));
+            Console.WriteLine(airfield.AddDrone(fourthDrone));
+            Console.WriteLine(airfield.AddDrone(fifthDrone)); 
+            Console.WriteLine(airfield.AddDrone(sixtDrone));
+            Console.WriteLine(airfield.FlyDrone("CW4"));
+            Console.WriteLine("-----------------FlyDronesByRange-----------------");
+            List<Drone> flyDrones = airfield.FlyDronesByRange(10);
+            foreach (var droneToFly in flyDrones)
+            {
+                Console.WriteLine(droneToFly);
+            }
+            Console.WriteLine(airfield.RemoveDroneByBrand("Cheerwing"));
+            Console.WriteLine("----------------------Report----------------------");
+            Console.WriteLine(airfield.Report());
+        }
+    }
+}
