87758ead199da9499120be78171d5d16c1d249f7
Rosen Yordanov <roskata182003@gmail.com>
2022-10-16 13:09:22 +0300

    Upload Iterators and Comparators Lab

---
+++ Iterators and Comparators Lab/BookComparer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IteratorsAndComparators
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library library = new Library(bookOne, bookTwo, bookThree);
+            foreach (Book book in library)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
