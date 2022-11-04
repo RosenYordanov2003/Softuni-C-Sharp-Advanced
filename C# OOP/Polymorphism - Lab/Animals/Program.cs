df9f3815cda2db071f95aa24712ba92bc9b4db0d
Rosen Yordanov <roskata182003@gmail.com>
2022-11-04 11:28:26 +0200

    Upload Polymorphism - Lab from C# OOP course

---
+++ C# OOP/Polymorphism - Lab/Animals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = null;
+            animal = new Cat("Peter", "Whiskas");
+            Console.WriteLine(animal.ExplainSelf());
+            animal = new Dog("George", "Meat");
+            Console.WriteLine(animal.ExplainSelf());
+        }
+    }
+}
---
+++ C# OOP/Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Width + 2 * Height;
+        }
+        public override string Draw()
+        {
+            return $"{base.Draw()} {this.GetType().Name}";
+        }
+    }
+}
