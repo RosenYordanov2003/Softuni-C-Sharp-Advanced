8f6f3641a5817256a39aedd26ca790410cf2287e
Rosen Yordanov <roskata182003@gmail.com>
2022-11-15 19:50:27 +0200

    Upload Reflection and Attributes - Lab

---
+++ C# OOP/Reflection and Attributes - Lab/Stealer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Stealer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+
+}
---
+++ C# OOP/Reflection and Attributes - Lab/Stealer/Spy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] parameters)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type classType = Type.GetType(className);
+            FieldInfo[] fileds = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+            sb.AppendLine($"Class under investigation: {classType.FullName}");
+            foreach (FieldInfo field in fileds.Where(x => parameters.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+            return sb.ToString().Trim();
+        }
+       
+    }
+}
